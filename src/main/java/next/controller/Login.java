369ee4a8612b14003c9b34e66270b8cc2b98a193
John Grib <johngrib@woowahan.com>
2017-04-18 23:27:13 +0900

    login 컨트롤러에 annotation 적용

---
+++ src/main/java/next/controller/Login.java
@@ -0,0 +1,44 @@
+package next.controller;
+
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by johngrib on 2017. 4. 18..
+ */
+@annotation.Controller(name = "/users/login")
+public class Login extends Controller {
+
+    private static final Logger log = LoggerFactory.getLogger(Login.class);
+
+    @Override
+    public void exec(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+        User user = DataBase.findUserById(userId);
+        if (user == null) {
+            req.setAttribute("loginFailed", true);
+            RequestDispatcher rd = req.getRequestDispatcher("/user/login.jsp");
+            rd.forward(req, resp);
+            return;
+        }
+
+        if (user.matchPassword(password)) {
+            HttpSession session = req.getSession();
+            session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
+            resp.sendRedirect("/");
+        } else {
+            req.setAttribute("loginFailed", true);
+            RequestDispatcher rd = req.getRequestDispatcher("/user/login.jsp");
+            rd.forward(req, resp);
+            return;
+        }
+    }
+}
