49d4e15fa311beaa3c7f9d2125e572fd72b96afe
John Grib <johngrib@woowahan.com>
2017-04-18 23:25:15 +0900

    ListUser 컨트롤러에 annotation 적용

--- src/main/java/next/controller/ListUser.java
+++ src/main/java/next/controller/ListUser.java
@@ -1,21 +1,23 @@
 package next.controller;
 
 import core.db.DataBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet("/users")
-public class ListUserController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+/**
+ * Created by johngrib on 2017. 4. 18..
+ */
+@annotation.Controller(name = "/users")
+public class ListUser extends Controller {
+
+    private static final Logger log = LoggerFactory.getLogger(ListUser.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void exec(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         if (!UserSessionUtils.isLogined(req.getSession())) {
             resp.sendRedirect("/users/loginForm");
             return;
