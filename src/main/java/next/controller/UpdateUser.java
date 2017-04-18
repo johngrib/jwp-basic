87fcc26825c0235cdf13dc995f60c03b1a561867
John Grib <johngrib@woowahan.com>
2017-04-18 23:43:01 +0900

    UpdateUser 컨트롤러에 annotation 적용

---
+++ src/main/java/next/controller/UpdateUser.java
@@ -0,0 +1,33 @@
+package next.controller;
+
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by johngrib on 2017. 4. 18..
+ */
+@annotation.Controller(name = "/users/update")
+public class UpdateUser extends Controller {
+
+    private static final Logger log = LoggerFactory.getLogger(UpdateUser.class);
+
+    @Override
+    public void exec(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        User user = DataBase.findUserById(req.getParameter("userId"));
+        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+
+        User updateUser = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+                req.getParameter("email"));
+        log.debug("Update User : {}", updateUser);
+        user.update(updateUser);
+        resp.sendRedirect("/");
+    }
+}
---
+++ src/main/java/next/controller/UpdateUserForm.java
@@ -0,0 +1,31 @@
+package next.controller;
+
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by johngrib on 2017. 4. 18..
+ */
+@annotation.Controller(name = "/users/updateForm")
+public class UpdateUserForm extends Controller {
+
+    private static final Logger log = LoggerFactory.getLogger(UpdateUserForm.class);
+
+    @Override
+    public void exec(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+        req.setAttribute("user", user);
+        RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
+        rd.forward(req, resp);
+    }
+}
