a08eff7aae633ca5418231a648d3bdad26f09b2e
John Grib <johngrib@woowahan.com>
2017-04-25 20:19:25 +0900

    [요구사항 4] 로그인한 사용자만 질문이 가능하도록 수정한다.

--- src/main/java/next/controller/user/CreateQuestionController.java
+++ src/main/java/next/controller/user/CreateQuestionController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class CreateQuestionController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(CreateQuestionController.class);
@@ -20,14 +21,23 @@ public class CreateQuestionController extends AbstractController {
     @Override
     public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+        final User loginUser = (User) request.getSession().getAttribute("user");
+        log.debug("login user : {}", loginUser);
+
+        if (loginUser == null) {
+            log.debug("not login user");
+            return jspView("redirect:/");
+        }
+
         final Question question = new Question(
-                request.getParameter("writer"),
+                loginUser.getName(),
                 request.getParameter("title"),
                 request.getParameter("contents")
         );
 
         log.debug("Question : {}", question);
         dao.insert(question);
+
         return jspView("redirect:/");
     }
 }
